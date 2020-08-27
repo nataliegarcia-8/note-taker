c7721bb654399edf52e2308d196c6e14f3cca521
Natalie Garcia <gnatalie.booking@gmail.com>
2020-08-27 14:04:12 -0400

    cleaning up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,38 +2,27 @@ const fs = require('fs')
 const path = require('path')
 const DB_PATH = path.join(__dirname, '../db/db.json')
 
-/**
- * @returns {any[]} Notes from Database
- */
-const fetchDB = () => JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
-
-/**
- * 
- * @param {Object} note New Note for Database 
- * @returns {void} void
- */
-const writeDB = (note) => fs.writeFileSync(DB_PATH, JSON.stringify(note))
-
-module.exports = function (app) {
-  app.get("/api/notes", function (req, res) {
-    const notesData = fetchDB()
-
+module.exports = function(app) {
+  app.get("/api/notes", function(req, res) {
+    const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
+    
     res.json(notesData);
   });
+  
+  
+  app.post("/api/notes", function(req, res) {
 
-
-  app.post("/api/notes", function (req, res) {
-    const notesData = fetchDB()
+    const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
     const newNote = req.body
-
+    
     notesData.push(newNote)
 
-    writeDB(newNote)
-
+    fs.writeFileSync(DB_PATH, JSON.stringify(notesData))
+ 
     res.sendStatus(200)
   });
 
-  app.delete("/api/notes/:id", function (req, res) {
-    console.log(req.params.id)
+  app.delete("/api/notes/:id", function(req, res) {
+   console.log(req.params.id)
   });
 };
