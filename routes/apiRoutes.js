f67f9759d93e68e7d429e28cf36a7f48f43b3d8f
Natalie Garcia <gnatalie.booking@gmail.com>
2020-08-27 23:11:19 -0400

    delete button functioning, cleaned up code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,7 +5,6 @@ const DB_PATH = path.join(__dirname, '../db/db.json')
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
     const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
-    console.log(notesData)
     res.json(notesData);
   });
   
@@ -13,17 +12,18 @@ module.exports = function(app) {
   app.post("/api/notes", function(req, res) {
 
     const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
-    console.log(notesData)
     const newNote = req.body
     notesData.push(newNote)
-    console.log(newNote)
 
     fs.writeFileSync(DB_PATH, JSON.stringify(notesData))
-    console.log(fs.writeFileSync(DB_PATH, JSON.stringify(notesData)))
     res.sendStatus(200)
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-   console.log(req.params.id)
+   const notesData = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'))
+   const savedNotes = notesData.filter((note) => {
+     return note.id !== req.params.id})
+   const newNotes = fs.writeFileSync(DB_PATH, JSON.stringify(savedNotes))
+   res.send(newNotes);
   });
 };
